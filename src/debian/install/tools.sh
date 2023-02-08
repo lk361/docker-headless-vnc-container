f88857b2a452224b0969ce49c710d81d5e4b47c1
zhm <comdot@qqcom>
2023-02-08 11:56:42 +0800

    1

--- src/debian/install/tools.sh
+++ src/debian/install/tools.sh
@@ -7,14 +7,14 @@ echo 'deb https://mirrors.tuna.tsinghua.edu.cn/debian/ sid main contrib non-free
 
 export http_proxy=http://192.168.4.1:7890
 export https_proxy=http://192.168.4.1:7890
+apt-get update
 apt-get install -y apt-transport-https ca-certificates
 export -n http_proxy
 export -n https_proxy
 
 echo -e "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ sid main contrib non-free\n" > /etc/apt/sources.list
-
-echo "Install some common tools for further installation"
 apt-get update
+echo "Install some common tools for further installation"
 apt-get install -y vim wget net-tools locales bzip2 procps \
     python3-numpy #used for websockify/novnc
 apt-get clean -y
