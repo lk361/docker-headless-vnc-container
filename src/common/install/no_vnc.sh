61a353b4b7f4306acd0d1906dcc132c0a7db57da
zhm <comdot@qqcom>
2023-02-08 11:38:44 +0800

    漏洞修复

--- src/common/install/no_vnc.sh
+++ src/common/install/no_vnc.sh
@@ -5,9 +5,14 @@ set -u
 
 echo "Install noVNC - HTML5 based VNC viewer"
 mkdir -p $NO_VNC_HOME/utils/websockify
+
+export http_proxy=http://192.168.4.1:7890
+export https_proxy=http://192.168.4.1:7890
 wget -qO- https://github.com/novnc/noVNC/archive/refs/tags/v1.3.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
 wget -qO- https://github.com/novnc/websockify/archive/refs/tags/v0.10.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
+export -n http_proxy
+export -n https_proxy
 #chmod +x -v $NO_VNC_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_lite.html`
 ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
--- src/debian/install/tools.sh
+++ src/debian/install/tools.sh
@@ -2,6 +2,18 @@
 ### every exit != 0 fails the script
 set -e
 
+
+RUN echo 'deb https://mirrors.tuna.tsinghua.edu.cn/debian/ sid main contrib non-free'\
+> /etc/apt/sources.list
+
+export http_proxy=http://192.168.4.1:7890
+export https_proxy=http://192.168.4.1:7890
+apt-get install -y apt-transport-https ca-certificates
+export -n http_proxy
+export -n https_proxy
+
+echo -e "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ sid main contrib non-free\n" > /etc/apt/sources.list
+
 echo "Install some common tools for further installation"
 apt-get update
 apt-get install -y vim wget net-tools locales bzip2 procps \
